8b891450411506056a7acd1db79a0d2f87752b47
ketanmb69 <badhe.ketan@gmail.com>
2021-12-06 23:17:45 +0000

    added scripts and updated run.sh

---
+++ before_install.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+#Before Starting the Application
+
+pip install -r requirements.txt
+timeout 10
+
+export API_KEY=pk_2417e95c90454964b39de4be237537de
--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!bin/bash
 
-pip install -r requirements.txt
-timeout 10
-
-export API_KEY=pk_2417e95c90454964b39de4be237537de
-
+#Run the application
 
 python application.py &
