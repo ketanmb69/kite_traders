52b8ceff7dfdd308cb895afed826667c2238bb65
ketanmb69 <badhe.ketan@gmail.com>
2021-12-07 01:10:54 +0000

    updated appspec.yml

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 sudo chmod -R 775 /home/ec2-user/kite-traders
 
 cd /home/ec2-user/kite-traders
-pip3 install requirements.txt
+pip3 install -r requirements.txt
 
 echo "Running the application"
 python3 application.py & 
