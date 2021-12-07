ca71d96e5639d87f18e8606e094c241033bce38a
ketanmb69 <badhe.ketan@gmail.com>
2021-12-07 01:18:50 +0000

    updated scripts

--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ sudo chmod -R 775 /home/ec2-user/kite-traders
 cd /home/ec2-user/kite-traders
 pip3 install -r requirements.txt
 
+
+export API_KEY="pk_2417e95c90454964b39de4be237537de"
+
 echo "Running the application"
 python3 application.py & 
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,11 +3,11 @@
 
 
 DIR="/home/ec2-user/kite-traders"
+
+
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
-
-export API_KEY=pk_2417e95c90454964b39de4be237537de
