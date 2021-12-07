ad8f95b39ee1798514851fc375dd7770782f2011
ketanmb69 <badhe.ketan@gmail.com>
2021-12-07 10:32:49 +0000

    updated run.sh and appspec.yml

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 
 #Run the application
 
-sudo chmod -R 775 /home/ec2-user/kite-traders
+sudo chmod -R 775 /home/ec2-user/kite-traders1
 
-cd /home/ec2-user/kite-traders
+cd /home/ec2-user/
+python3 -m venv env
+source /home/ec2-user/env/bin/activate
+
+cd /home/ec2-user/kite-traders1
 pip3 install -r requirements.txt
 
 
 export API_KEY="pk_2417e95c90454964b39de4be237537de"
 
 echo "Running the application"
-python3 application.py & 
+python3 application.py > app.out.log 2> app.err.log < /dev/null & 
