67a38460300565ebb328e9f5751ff52ce7e16906
ketanmb69 <badhe.ketan@gmail.com>
2021-12-08 17:25:25 +0000

    updated about.html

--- run.sh
+++ run.sh
@@ -12,38 +12,10 @@ source /home/ubuntu/kite-traders1/venv/bin/activate
 cd /home/ubuntu/kite-traders1/myproj
 pip install -r requirements.txt
 
+sudo chown ubuntu:ubuntu finance.db
+
 export API_KEY="pk_2417e95c90454964b39de4be237537de"
 
 echo "Running the application"
 python application.py >> /dev/null 2>&1 &
 
-sudo service codedeploy-agent stop
-
-sudo service codedeploy-agent start
-
-#echo "removing the code-agent"
-#sudo dpkg --purge codedeploy-agent
-
-#echo "deleting the codeagent dir"
-#sudo rm -rf /opt/codedeploy-agent/
-
-#echo "killing teh codeagent processes"
-#sudo ps -fu root | grep codedeploy | awk -F" " '{print $2}' | xargs sudo kill
-
-
-#if sudo ps -fu 'root' | grep codedeploy
-#then
-#PID=`sudo ps -fu root | grep codedeploy | awk -F" " '{print $2}' | head -1`; 
-#sudo kill -9 $PID; 
-#while [ -e /proc/$PID ]; 
-#do echo "Process: $PID is still running" > /home/ubuntu/pid.log;
-#sleep .6; 
-#done; 
-#echo "Process $PID has finished" >> /home/ubuntu/pid.log; 
-#else
-#echo "no process running as codeagent"
-#fi
-
-#echo "installing the codeagent"
-#cd /home/ubuntu/
-#sudo ./install auto > /dev/null 2>&1 &
