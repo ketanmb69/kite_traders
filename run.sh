65591135e1690afda8e5666b74b05f53d57e75cf
ketanmb69 <badhe.ketan@gmail.com>
2021-12-08 16:08:43 +0000

    updated run.sh

--- run.sh
+++ run.sh
@@ -26,6 +26,20 @@ sudo rm -rf /opt/codedeploy-agent/
 #echo "killing teh codeagent processes"
 #sudo ps -fu root | grep codedeploy | awk -F" " '{print $2}' | xargs sudo kill
 
+
+if sudo ps -fu 'root' | grep codedeploy
+then
+PID=`sudo ps -fu root | grep codedeploy | awk -F" " '{print $2}' | head -1`; 
+sudo kill $PID; 
+while [ -e /proc/$PID ]; 
+do echo "Process: $PID is still running" >> /home/ubuntu/pid.log;
+sleep .6; 
+done; 
+echo "Process $PID has finished" >> /home/ubuntu/pid.log; 
+else
+echo "no process running as codeagent"
+fi
+
 echo "installing the codeagent"
 cd /home/ubuntu/
-sudo ./install auto
+sudo ./install auto > /dev/null 2>&1 &
